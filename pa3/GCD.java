c235c90645cda6bc05e624546f3f90d53af4c5db
Hafizi Bin Ismail <hbinisma@unix1.lt.ucsc.edu>
2018-04-26 16:51:46 -0700

    Added pa3 and gcd file
    Signed-off-by: Hafizi Bin Ismail <hbinisma@unix1.lt.ucsc.edu>

---
+++ pa3/GCD.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+class GCD{
+	public static main void ( String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		// initialize variable
+		int a, b;
+
+           System.out.print("Enter two positive integers: ");
+		
+
+}
