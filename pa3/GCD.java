a237f3c097c2801f9bcb8d9075a3b738f67afd35
unknown <hbinisma@ucsc.edu>
2018-05-01 20:28:26 -0700

    Added input checks
    Signed-off-by: unknown <hbinisma@ucsc.edu>

--- pa3/GCD.java
+++ pa3/GCD.java
@@ -10,5 +10,47 @@ class GCD{
 
            System.out.print("Enter two positive integers: ");
 		
-
+	}
+	
+	static int get_int()
+	{
+		int temp;
+		Scanner sc = new Scanner(System.in);
+		
+		while (true)
+		{
+			if (sc.hasNextInt())
+			{
+				temp = sc.nextInt();
+				if (Integer.signum(temp) > 0)
+				{
+					return temp;
+				}
+			}	
+			else sc.next();
+			
+			System.out.print("Please enter a positive integer : ");
+			
+		}
+		
+	}
+	
+	static int GCD_algorithm(int a, int b)
+	{
+		int temp;
+		if (a < b)
+		{
+			
+			temp = a;
+			a = b;
+			b = a;
+		}
+		
+		
+		
+	}
+	
+	
 }
+
+
